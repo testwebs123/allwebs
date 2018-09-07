a05c49e9857fc52c7d688c61029f6a5bc6675fa3
zql1234 <951212526@qq.com>
2018-09-07 09:28:12 +0800

    first commit

--- szggzyjy/a1e04c2c-3ceb-4505-a4fc-590a50e6fc7b&CategoryNum=002004001002/js/count.js
+++
@@ -1,2 +0,0 @@
-
-document.write('<img src="/szfront/Images/Count/6.gif"><img src="/szfront/Images/Count/8.gif"><img src="/szfront/Images/Count/3.gif"><img src="/szfront/Images/Count/6.gif"><img src="/szfront/Images/Count/7.gif"><img src="/szfront/Images/Count/8.gif"><img src="/szfront/Images/Count/5.gif"><img src="/szfront/Images/Count/1.gif">');
