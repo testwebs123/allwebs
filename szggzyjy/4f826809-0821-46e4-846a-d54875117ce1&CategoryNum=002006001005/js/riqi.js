f908897a56e93969cae5af17123ea68e2ad8ee6d
zql1234 <951212526@qq.com>
2018-09-11 15:12:41 +0800

    1
    1

---
+++ szggzyjy/4f826809-0821-46e4-846a-d54875117ce1&CategoryNum=002006001005/js/riqi.js
@@ -0,0 +1,8 @@
+﻿$(function($) {
+    var today=new Date();
+    var d=new Array("星期日","星期一","星期二","星期三","星期四","星期五","星期六");
+    var AAAA=today.getFullYear()+"年"+(today.getMonth()+1)+"月"+today.getDate()+"日";
+    AAAA =""+AAAA + " " +  d[today.getDay()];
+    $("#riqi").html(AAAA);
+    $("#riqi1").html(AAAA); $("#riqi2").html(AAAA);
+})
